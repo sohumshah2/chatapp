3e3670827cb9ad9a7b46eafb1b2eebd2ce9f5233
sohumshah2 <sohumshah2@gmail.com>
2023-10-12 02:20:39 +1100

    dm functionality started, not working rn

--- src/App.js
+++ src/App.js
@@ -1,47 +1,113 @@
 import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
 
-const socket = io("http://server.sharedwithexpose.com"); // Replace with your server's URL
+const socket = io("localhost:5050"); // Replace with your server's URL
 // const socket = io("http://server.sharedwithexpose.com/", {
 //   transports: ["websocket"],
 // });
 
 function App() {
+  const [sender, setSender] = useState("");
+  const [receiver, setReceiver] = useState("");
   const [messages, setMessages] = useState([]);
   const [message, setMessage] = useState("");
 
   useEffect(() => {
     socket.on("broadcastMessage", (msg) => {
-      setMessages([...messages, msg]);
+      console.log("received a new msg", msg);
+      console.log(
+        "received a new message, messages:",
+        "messages: ",
+        messages,
+        "sender: ",
+        sender,
+        sender.length,
+        "receiver: ",
+        receiver
+      );
+      if (sender === "a") {
+        console.log("bruh");
+      }
+      if (msg.receiver === sender) {
+        console.log("yipe");
+        setMessages([...messages, msg]);
+      }
     });
 
     return () => {
       // socket.disconnect();
     };
-  }, [messages]);
+  }, []);
 
   const handleSendMessage = () => {
     console.log("Send button clicked");
     console.log(message);
-    socket.emit("sendMessage", message);
-    setMessage("");
+    socket.emit("sendMessage", {
+      sender: sender,
+      receiver: receiver,
+      message: message,
+    });
+    // setMessage("");
   };
 
   return (
     <div className="App">
       <h1>Chat App</h1>
+      <p>
+        for debugging - sender: {sender}, receiver: {receiver}
+      </p>
       <div className="message-container">
-        {messages.map((msg, index) => (
-          <div key={index}>{msg}</div>
-        ))}
+        <p style={{ fontWeight: "bold" }}>Messages:</p>
+        {messages
+          .filter((msg) => msg.sender != sender)
+          .map((msg, index) => (
+            <div key={index}>
+              {msg.sender}: {msg.message}
+            </div>
+          ))}
       </div>
       <div className="input-container">
-        <input
-          type="text"
+        <p style={{ fontWeight: "bold" }}>Enter your username:</p>
+        <input onChange={(e) => setSender(e.target.value)}></input>
+        {/* <span
+          contentEditable="true"
           value={message}
-          onChange={(e) => setMessage(e.target.value)}
-        />
-        <button onClick={handleSendMessage}>Send</button>
+          style={{
+            display: "inline-block",
+            border: "1px solid black",
+            minWidth: "100px",
+            maxWidth: "200px",
+          }}
+          onInput={(e) => setSender(e.target.textContent)}
+        ></span> */}
+        <p style={{ fontWeight: "bold" }}>Enter receiver username:</p>
+        <span
+          contentEditable="true"
+          value={message}
+          style={{
+            display: "inline-block",
+            border: "1px solid black",
+            minWidth: "100px",
+            maxWidth: "200px",
+          }}
+          onInput={(e) => setReceiver(e.target.textContent)}
+        ></span>
+
+        <p style={{ fontWeight: "bold" }}>Enter Message:</p>
+        <span
+          contentEditable="true"
+          value={message}
+          style={{
+            display: "inline-block",
+            border: "1px solid black",
+            minWidth: "200px",
+            maxWidth: "200px",
+          }}
+          onInput={(e) => setMessage(e.target.textContent)}
+        ></span>
+        <div>
+          <button onClick={handleSendMessage}>Send</button>
+        </div>
       </div>
     </div>
   );
